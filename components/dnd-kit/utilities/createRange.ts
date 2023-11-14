152f0c262dfe0a3a8c997a8b730253b242aa5a71
devanondo <dev.abormon28@gmail.com>
2023-11-14 14:52:12 +0600

    [REMOVED] REMOVED THE DND-KIT ALL FILES

--- components/dnd-kit/utilities/createRange.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-const defaultInitializer = (index: number) => index
-
-export function createRange<T = number>(
-    length: number,
-    initializer: (index: number) => any = defaultInitializer
-): T[] {
-    return [...new Array(length)].map((_, index) => initializer(index))
-}
